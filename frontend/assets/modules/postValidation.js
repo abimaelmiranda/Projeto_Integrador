0845cfbe71808737cf0c66a809f0eb760496a6b2
Abimael Miranda <145388823+abimaelmiranda@users.noreply.github.com>
2024-07-04 22:55:04 -0300

    Update 04-07-2024 #3
    Adicionado validações ao campo de ingredientes para prevenir registros errados no banco

--- frontend/assets/modules/postValidation.js
+++ frontend/assets/modules/postValidation.js
@@ -21,19 +21,27 @@ export default class PostValidation {
         const shortDescription = el.querySelector('.shortDescription');
         const ingredients = el.querySelector('.ingredients');
         const preparationMethod = el.querySelector('.preparationMethod');
-        const ingredientsValue = ingredients.value.split('\n').map(ingredient => ingredient.trim());
+        const ingredientsValue = ingredients.value.split('\n').map(ingredient => ingredient.trim()).filter(ingredient => ingredient.length > 0);
         const ingredientsArray = el.querySelector('.ingredientsArray');
         const image = el.querySelector('.imageFile').files[0];
         const csrf = document.querySelector('input[name=_csrf]').value;
 
-        console.log(image);
 
         let error = false;
 
+        if (ingredientsValue.length === 0) {
+            error = true;
+        }
+    
+        if (!ingredients.value.trim()) {
+            error = true;
+        }
+    
         ingredientsArray.value = JSON.stringify(ingredientsValue);
-        if(!error) el.submit();
-
-   
-
+        
+        if(!error) {
+            // el.submit();
+            console.log(ingredientsValue);
+        }
     }
 }
